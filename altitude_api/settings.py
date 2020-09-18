300adf6b156c77c736a7545b7b4edf07552cad95
Vicki Mes <kemboivictor@outlook.com>
2020-09-18 11:44:58 +0300

    Changed statios files dir and static url in settings.py

--- altitude_api/settings.py
+++ altitude_api/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
-STATIC_URL = '/static/'
+STATIC_URL = '/static_files/'
 
 
 # Extra lookup directories for collectstatic to find static files
