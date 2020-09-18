8f44c8a6ce8a0a86ebaba41728ec6192e54b0277
Vicki Mes <kemboivictor@outlook.com>
2020-09-18 16:19:02 +0300

    Prod static files.

--- altitude_api/settings.py
+++ altitude_api/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files') 
 STATIC_URL = '/static/'
 
 
