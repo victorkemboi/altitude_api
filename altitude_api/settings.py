f017af2e4873add394e864ed99e7604cb7114eb5
Vicki Mes <kemboivictor@outlook.com>
2020-09-18 11:35:25 +0300

    Updated static files folder in settings.py

--- altitude_api/settings.py
+++ altitude_api/settings.py
@@ -137,4 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATIC_URL = '/static/'
+
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
